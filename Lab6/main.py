7195a6332568668365ccbfe61f71e6ed9d3538ff
Eric Charnesky <eric.charnesky@careevolution.com>
2021-11-11 15:51:18 -0500

    Lab 6

---
+++ Lab6/main.py
@@ -0,0 +1,49 @@
+def menu():
+    print("DO you want to?")
+    print("1 - Add a student")
+    print("2 - Add an assignment")
+    print("3 - Score assignment")
+    print("4 - print the gradebook")
+    print("5 - quit")
+    choice = input()
+    return choice
+
+gradebook = {}
+
+choice = ""
+
+while choice != "5":
+    choice = menu()
+    if choice == "1":
+        name = input("Enter the student's name: ")
+        gradebook[name] = {}
+
+        if len(gradebook.keys()) > 1:
+            for student in gradebook:
+                if student != name:
+                    for assignment in gradebook[student]:
+                        if "points_possible" in assignment:
+                            gradebook[name][assignment] = gradebook[student][assignment]
+                        elif "score" in assignment:
+                            gradebook[name][assignment] = 0
+                    break # stop looping through other student keys
+
+    elif choice == "2":
+        assignment_name = input("Enter the assignment's name: ")
+        points_possible = int(input("How many points is this assignment worth?"))
+
+        for student in gradebook:
+            gradebook[student][assignment_name + "_points_possible"] = points_possible
+            gradebook[student][assignment_name + "_score"] = 0
+
+    elif choice == "3":
+        assignment_to_score = input("What assignment did you want to score?")
+        for student in gradebook:
+            print(assignment_to_score, "points possible: ", gradebook[student][assignment_to_score + "_points_possible"])
+            score = int(input("What was the score for " + student + ": "))
+            gradebook[student][assignment_to_score + "_score"] = score
+
+    elif choice == "4":
+        for student in gradebook:
+            print(student, gradebook[student])
+
