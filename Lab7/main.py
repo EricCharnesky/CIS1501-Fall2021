2dbf78d32e109d666bc7c1b6ca113f1ced0fd990
Eric Charnesky <eric.charnesky@careevolution.com>
2021-11-18 16:31:52 -0500

    lab 7

--- Lab7/main.py
+++ Lab7/main.py
@@ -1,7 +1,48 @@
 class Assignment:
 
-    def __init__(self, name):
+    def __init__(self, name, points_possible, points_earned):
         self._name = name
+        self._points_possible = points_possible
+        self.set_points_earned(points_earned)
 
     def get_name(self):
         return self._name
+
+    def get_points_possible(self):
+        return self._points_possible
+
+    def get_points_earned(self):
+        return self._points_earned
+
+    def get_letter_grade(self):
+        percentage = self._points_earned / self._points_possible
+        if percentage >= .93:
+            return "A"
+        elif percentage >= .90:
+            return "A-"
+        elif percentage >= .87:
+            return "B+"
+        elif percentage >= .83:
+            return "B"
+        elif percentage >= .80:
+            return "B-"
+        elif percentage >= .77:
+            return "C+"
+        elif percentage >= .73:
+            return "C"
+        elif percentage >= .70:
+            return "C-"
+        elif percentage >= .67:
+            return "D+"
+        elif percentage >= .63:
+            return "D"
+        else:
+            return "F"
+
+    def set_points_earned(self, points_earned):
+        if points_earned < 0:
+            self._points_earned = 0
+        elif points_earned > self._points_possible:
+            self._points_earned = self._points_possible
+        else:
+            self._points_earned = points_earned
--- Lab7/test_main.py
+++ Lab7/test_main.py
@@ -7,10 +7,66 @@ class TestAssignment(TestCase):
     def test_init(self):
         # arrange
         expected_name = 'Lab7'
+        expected_points_possible = 10
+        expected_earned = 10
 
         # act
-        assignment = Assignment(expected_name)
+        assignment = Assignment(expected_name, expected_points_possible, expected_earned)
         actual_name = assignment.get_name()
+        actual_points_possible = assignment.get_points_possible()
+        actual_points_earned = assignment.get_points_earned()
 
         # assert
         self.assertEqual(expected_name, actual_name)
+        self.assertEqual(expected_points_possible, actual_points_possible)
+        self.assertEqual(expected_earned, actual_points_earned)
+
+    def test_set_points_earned_negative(self):
+        # arrange
+        points_possible = 10
+        expected_earned = 0
+
+        # act
+        assignment = Assignment("", points_possible, -1)
+        actual_points_earned = assignment.get_points_earned()
+
+        # assert
+        self.assertEqual(expected_earned, actual_points_earned)
+
+    def test_set_points_earned_more_than_possible(self):
+        # arrange
+        points_possible = 10
+        expected_earned = 10
+
+        # act
+        assignment = Assignment("", points_possible, 20)
+        actual_points_earned = assignment.get_points_earned()
+
+        # assert
+        self.assertEqual(expected_earned, actual_points_earned)
+
+    def test_get_letter_grade_a(self):
+        # arrange
+        points_possible = 10
+        points_earned = 10
+        expected_letter_grade = 'A'
+
+        # act
+        assignment = Assignment("", points_possible, points_earned)
+        actual_letter_grade = assignment.get_letter_grade()
+
+        # assert
+        self.assertEqual(expected_letter_grade, actual_letter_grade)
+
+    def test_get_letter_grade_cplus(self):
+        # arrange
+        points_possible = 100
+        points_earned = 78
+        expected_letter_grade = 'C+'
+
+        # act
+        assignment = Assignment("", points_possible, points_earned)
+        actual_letter_grade = assignment.get_letter_grade()
+
+        # assert
+        self.assertEqual(expected_letter_grade, actual_letter_grade)
